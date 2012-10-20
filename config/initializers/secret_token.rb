55db61131383b95898f71b40794f3a0490457e8d
antoshalee <antoshalee@gmail.com>
2012-10-20 11:43:38 +0800

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foresight::Application.config.secret_token = 'e27a4640b291c922bdae4dec8f58f99940708472b0e14894e5c012db78b029459a9a8ec84f6534081f4408e123c6b08223ab45e9b09cb8f51f36f58cc36b5476'
