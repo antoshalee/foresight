dd3004c2b5942047e62033bdfd945fe3f709bc89
antoshalee <antoshalee@gmail.com>
2012-10-20 18:34:23 +0800

    uniqueness validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,9 @@
 class Member < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  validates :vkontakte_uid, uniqueness: {allow_nil: true}
+  validates :facebook_uid, uniqueness: {allow_nil: true}
+
   def name
     "#{first_name} #{last_name}"
   end
