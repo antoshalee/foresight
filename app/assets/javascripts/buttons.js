68ee041a9c124d22be8e3270844a38b07901447c
antoshalee <antoshalee@gmail.com>
2012-10-23 14:35:00 +0800

    vk like buttons

--- app/assets/javascripts/buttons.js
+++ app/assets/javascripts/buttons.js
@@ -1,21 +1,38 @@
-$(document).ready(function(){
+initLikeButtons = function() {
 
 	//document.getElementById('vk_share_button').innerHTML = VK.Share.button('http://mysite.com', {type: 'link'});
-	$('.member_share_button').each(function(){
-		var memberId = ($(this).attr('data-id'));
-		var memberImage = ($(this).attr('data-image'));
-		var memberDescription = ($(this).attr('data-description'));
-		$(this).html(VK.Share.button(
-			{
-				url:'http://kmfuture.herokuapp.com?member='+memberId,
-				title: 'Голосуй за участника Foresight',
-				description: memberDescription,
-				image: memberImage,
-				noparse: true
-			},
-			{type: 'round'
-			})
-		);
-	});
 
-});
+	$('.vk_like').each(function(){
+		var divId = $(this).attr('id');
+		var memberPhoto = $(this).attr('data-image');
+		var memberId = $(this).attr('data-id');
+		VK.Widgets.Like(
+			divId,
+			{
+				type: 'button',
+				pageImage: memberPhoto,
+				pageUrl: 'http://kmfuture.herokuapp.com/'
+			},
+			memberId
+		);
+	})
+
+
+	// $('.member_share_button').each(function(){
+	// 	var memberId = ($(this).attr('data-id'));
+	// 	var memberImage = ($(this).attr('data-image'));
+	// 	var memberDescription = ($(this).attr('data-description'));
+	// 	$(this).html(VK.Share.button(
+	// 		{
+	// 			url:'http://kmfuture.herokuapp.com/member/'+memberId,
+	// 			title: 'Голосуй за участника Foresight',
+	// 			description: memberDescription,
+	// 			image: memberImage,
+	// 			noparse: true
+	// 		},
+	// 		{type: 'round'
+	// 		})
+	// 	);
+	// });
+
+};
