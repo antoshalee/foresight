141b6d186ad6478ac20656614eccb6cb2f876376
antoshalee <antoshalee@gmail.com>
2012-10-24 14:51:19 +0800

    dymanic index page

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,5 +1,12 @@
 class StartController < ApplicationController
+
+  layout "main"
+
   def index
+  end
+
+  def secret
+    render layout: 'application'
   end
 
   def rating
--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Foresight::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  get "secret"=> 'start#index'
+  get "secret"=> 'start#secret'
 
   root :to => 'start#index'
 
