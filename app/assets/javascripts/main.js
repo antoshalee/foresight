a1a4e45284491dadd6402818e990758448a117e9
antoshalee <antoshalee@gmail.com>
2012-10-25 15:32:06 +0800

    register popup

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	docW = $(document).width();
 	docH = $(document).height();
 	winW = $(window).width();
@@ -14,7 +14,10 @@ $(document).ready(function(){
 		'time' : '400',
 		'prev' : '.sliderexperts__prev',
 		'next' : '.sliderexperts__next'
-	})
+	});
+
+	// Sign up
+	popup__init();
 
 });
 
@@ -24,6 +27,8 @@ function commonHandlers() {
 		docH = $(document).height();
 		winW = $(window).outerWidth();
 		winH = $(window).height();
+
+		experts();
 	});
 
 	$('.sitenav__link').bind('click', function(){
@@ -42,4 +47,37 @@ function experts() {
 	var elemsScreenCount = Math.floor((winW-150)/elemsWidth);
 	var wrapperWidth = elemsScreenCount * elemsWidth;
 	$('.sliderexperts__wrap').css('width', wrapperWidth+'px');
+}
+
+
+function popup__init() {
+	var popup = $('.popup')
+	// Normalize indents
+	popup.each(function(i){
+		var el = $(this)
+		var mt = -0.5 * el.height()
+		var ml = -0.5 * el.width()
+		el.css({'margin':mt+'px 0 0 '+ml+'px'})
+	})
+	// Open popup
+	$('.js_popup').bind('click',function(){
+		var rel = $(this).attr('data-rel')
+		popup__show(rel)
+	})
+	// Close popup
+	$('#overlay, .popup_close').bind('click',function(){
+		$('#overlay, .popup').hide();
+	})
+}
+
+function popup__show(rel) {
+	$('#overlay, .popup').hide();
+	if ( $.browser.msie && parseInt($.browser.version, 10) < 9 ) $('#overlay, #'+rel).show()
+	else
+	$('#overlay, #'+rel)
+		.css({'opacity':0,'display':'block'})
+		.stop()
+		.animate({'opacity':1},300)
+
+	if ( $('.scrollpane_popup').size() ) $('.scrollpane_popup').jScrollPane()
 }
