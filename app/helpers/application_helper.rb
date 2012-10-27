567f03a04d7dee65b94bf9e0966f3fff4bd0d309
Chaptykov <tim.chaptykov@gmail.com>
2012-10-27 14:35:37 +0800

    Правки в экспертах

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,21 +3,21 @@ module ApplicationHelper
   def experts
     [
       [ 
-        "Александра Меркулова",
-        "Архитектор-урбанист, ведущий специалист, уполномоченный по развитию российских проектов KCAP Architects&Planners.",
-        "kcap.eu/ru/kcap/", nil, "http://www.facebook.com/alexandra.merkulova", "/assets/experts/merkulova.jpg"],
-      [ 
         "Илья Савчук",
-        "СО РАН, вице-президент Торговой палаты, г. Новосибирск",
+        "Директор по программам развития проектной группы «Метавер», соразработчик Форсайта «Образование 2030»",
         "http://metaver.ru", "http://vk.com/id14303795", "http://facebook.com/ilya.savchuk", "/assets/experts/savchuk.jpg"],
       [
         "Юрий Воронов",
-        "Генеральный директор консультационной фирмы «Корпус», вице-президент торгово-промышленной палаты, г. Новосибирск.",
+        "Генеральный директор консультационной фирмы «Корпус», вице-президент торгово-промышленной палаты.",
         nil, nil, nil, "/assets/experts/voronov.jpg"],
       [
         "Валерий Ефимов",
         "Директор Центра стратегических исследований и разработок Сибирского федерального университета",
-        "foresight.sfu-kras.ru", nil, "http://facebook.com/efimov.valerii", "/assets/experts/efimov.jpg"]
+        "foresight.sfu-kras.ru", nil, "http://facebook.com/efimov.valerii", "/assets/experts/efimov.jpg"],
+        [ 
+        "Александра Меркулова",
+        "Архитектор-урбанист, ведущий специалист, уполномоченный по развитию российских проектов KCAP Architects&Planners.",
+        "kcap.eu/ru/kcap/", nil, "http://www.facebook.com/alexandra.merkulova", "/assets/experts/merkulova.jpg"]
     ].map do |e|
       {name: e[0], role: e[1], web: e[2], vk_url: e[3], fb_url: e[4], img: e[5]}
     end
